e13a14dfa5786df2259c369fd3b1f71639e6a287
uwu <2077hl@gmail.com>
2025-06-25 01:09:36 +0300

    init variables

---
+++ variables.tf
@@ -0,0 +1,28 @@
+variable "yc_token" {
+  description = "Yandex Cloud OAuth token"
+  type        = string
+  sensitive   = true
+}
+
+variable "yc_cloud_id" {
+  description = "Yandex Cloud ID"
+  type        = string
+  sensitive   = true
+}
+
+variable "yc_folder_id" {
+  description = "Yandex Cloud Folder ID"
+  type        = string
+  sensitive   = true
+}
+
+variable "yc_zone" {
+  description = "Yandex Cloud default zone"
+  type        = string
+  default     = "ru-central1-a"
+}
+
+variable "subnet_id" {
+  description = "Subnet ID for VM"
+  type        = string
+}
